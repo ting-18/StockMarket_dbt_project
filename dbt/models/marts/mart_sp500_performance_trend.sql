1c3c5224ea501e32061841e06a24a1aba5cc1dda
Ting Liu <ting.liu.phd@gmail.com>
2025-03-31 20:23:25 +0000

    edit marts files

--- dbt/models/marts/mart_sp500_performance_trend.sql
+++ dbt/models/marts/mart_sp500_performance_trend.sql
@@ -8,3 +8,4 @@
 SELECT ticker, trade_date, opening_price, closing_price, lowest_price, highest_price, volume
 FROM {{ ref('stg_sp500_stockdata') }}
 WHERE ticker = "^GSPC" 
+ORDER BY trade_date DESC
